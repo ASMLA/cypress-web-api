0aa757a9d542ffc72a1f2580cfdd745aaa82a895
Andre <asmla123@gmail.com>
2024-12-22 04:53:26 -0300

    Ajustando popups imprevisíveis

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,6 +6,22 @@ Cypress.Commands.add('goToWikipediaWebsite', () => {
     cy.intercept('GET', '/portal/wikipedia.org/**').as('getHomeInfos');
     cy.visit('https://www.wikipedia.org');
     cy.wait('@getHomeInfos').its('response.statusCode').should('eq', 200);
+    // Intercepta a requisição do popup
+    cy.intercept('GET', '**/popup-endpoint').as('popupRequest');
+
+    // Interage com o botão ou ação que dispara o popup
+    cy.get('.trigger-button').click();
+
+    // Aguarda a requisição e interage com o popup
+    cy.wait('@popupRequest');
+    cy.get('.popup-selector', { timeout: 5000 })
+    .should('exist')
+    .then(() => {
+        cy.get('.popup-close-button').click();
+    })
+    .catch(() => {
+        cy.log('Popup não apareceu.');
+    });
     cy.compareSnapshot("home", {failSilently: true, errorThreshold: 1});
 })
 
