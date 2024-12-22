595e057e109326706ec86ba59c7e346cc3feeaf9
Andre <asmla123@gmail.com>
2024-12-22 05:23:51 -0300

    Corrigindo bug do popup no git actions

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,23 +5,22 @@ Cypress.Commands.add('goToWikipediaWebsite', () => {
     cy.clearCookies();
     cy.intercept('GET', '/portal/wikipedia.org/**').as('getHomeInfos');
     cy.visit('https://www.wikipedia.org');
-    cy.wait('@getHomeInfos').its('response.statusCode').should('eq', 200);
-    // Intercepta a requisição do popup
-    cy.intercept('GET', '**/popup-endpoint').as('popupRequest');
-
-    // Interage com o botão ou ação que dispara o popup
-    cy.get('.trigger-button').click();
-
-    // Aguarda a requisição e interage com o popup
-    cy.wait('@popupRequest');
-    cy.get('.popup-selector', { timeout: 5000 })
-    .should('exist')
-    .then(() => {
-        cy.get('.popup-close-button').click();
-    })
-    .catch(() => {
-        cy.log('Popup não apareceu.');
+    
+    // Verifica se o popup "Wikipedia is not for sale" aparece
+    cy.get('body').then(($body) => {
+        // Verifica se o popup está presente no DOM
+        if ($body.find('.wmn-dismissable').length > 0) {
+            cy.log('Popup encontrado, fechando...');
+            // Clica no botão de fechar do popup
+            cy.get('.wmn-dismissable .wikipedia-not-for-sale-close')
+                .click()
+                .should('not.exist'); // Garante que o popup foi fechado
+            } else {
+            cy.log('Nenhum popup encontrado.');
+            }
     });
+    
+    cy.wait('@getHomeInfos').its('response.statusCode').should('eq', 200);
     cy.compareSnapshot("home", {failSilently: true, errorThreshold: 1});
 })
 
